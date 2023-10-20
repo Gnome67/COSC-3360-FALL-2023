08aa72e9023da0ea52eebfeb75a43beab29bdcab
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-10-20 01:05:23 -0500

    Add files via upload

---
+++ PA2/client2.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+#include <memory>
+#include <queue>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <math.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+//Code is provided by Dr. Rincon from Canvas
+
+struct threader //for threading
+{
+    string inpStr; //the full unedited string
+    int ord; //the number of the thread (and CPU)
+    string outStr; //the final entropy string
+    char* serverIP; // argv[1]
+    char* portno; //argv[2]
+    threader(/*vector<pair<char, int>> eV,*/ string iS, int o, char* sIP, char* p) { //constructor
+        inpStr = iS;
+        ord = o;
+        serverIP = sIP;
+        portno = p;
+    }
+};
+
+//This function below stitches the output together in the format that Moodle expects
+
+string output(/*vector<pair<char, int>> entropyVector,*/ string CPUcount, string buffer, int cpu)
+{
+   string outputString = "CPU " + to_string(cpu+1), entropyString = "";
+   outputString += "\nTask scheduling information: ";
+   stringstream s(CPUcount);
+   char x; int y;
+   while(s >> x >> y) { outputString = outputString + x + "(" + to_string(y) + "), "; }
+   outputString.pop_back(); //remove ending space
+   outputString.pop_back(); //remove ending comma
+   outputString += "\nEntropy for CPU " + to_string(cpu+1)+"\n";
+   outputString += buffer;
+   return outputString;
+}
+
+//The threading function
+
+void* threadInstruct(void* arg)
+{
+    //initializing variables and casting
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    threader* arg_ptr = (threader*) arg;
+    int portno = atoi(arg_ptr->portno);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    string buffer = arg_ptr->inpStr;
+    //creating socket
+    if (sockfd < 0) { cerr << "ERROR opening socket" << endl; exit(0); }
+    server = gethostbyname(arg_ptr->serverIP);
+    if (server == NULL) { cerr << "ERROR, no such host" << endl; exit(0); }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) { cerr << "ERROR connecting" << endl; exit(0); }
+    //writing to server
+    int msgSize = buffer.size();
+    if (write(sockfd,&msgSize,sizeof(int)) < 0)  { cerr << "ERROR writing size to socket" << endl; exit(0); }
+    if (write(sockfd,buffer.c_str(),msgSize) < 0) { cerr << "ERROR writing string to socket" << endl; exit(0); }
+    //reading from server
+    if (read(sockfd,&msgSize,sizeof(int)) < 0) { cerr << "ERROR reading size from socket" << endl; exit(0); }
+    char *tempBuffer = new char[msgSize+1];
+    bzero(tempBuffer,msgSize+1);
+    if (read(sockfd,tempBuffer,msgSize) < 0) { cerr << "ERROR reading string from socket" << endl; exit(0); }
+    buffer = tempBuffer;
+    arg_ptr->outStr = output(arg_ptr->inpStr, buffer, arg_ptr->ord);
+    delete [] tempBuffer;
+    close(sockfd);
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    string input = "";
+    vector<string> cpuCounter;
+    vector<pthread_t> threadVector;
+    vector<threader*> structVector;
+    if (argc != 3) { cerr << "usage " << argv[0] << " hostname port" << endl; exit(0); }
+    while(getline(cin, input))
+    {
+        if(input.empty()) { break; }
+        cpuCounter.push_back(input);
+    }
+    //pthread create
+    for(int a = 0; a < cpuCounter.size(); a++) //variable-A threads
+    {
+        threader* newThread = new threader(/*allThreads[a],*/cpuCounter[a], a, argv[1], argv[2]);
+        pthread_t myThread;
+        if(pthread_create(&myThread, NULL, threadInstruct, static_cast<void*> (newThread)))
+        {
+            cout << "ERROR";
+            return -1;
+        }
+        structVector.push_back(newThread);
+        threadVector.push_back(myThread);
+    }
+    //pthread join
+    for(int b = 0; b < threadVector.size(); b++) { pthread_join(threadVector[b], NULL); }
+    //output
+    for(int c = 0; c < structVector.size(); c++) { cout << structVector[c]->outStr << endl << endl; }
+    return 0;
+}
---
+++ PA2/server2.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+#include <memory>
+#include <queue>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <math.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+//Code is provided by Dr. Rincon from Canvas
+
+void fireman(int) { while (waitpid(-1, NULL, WNOHANG) > 0) {;} }
+
+string calculateEntropy(vector<pair<char, int>> entropyVector)
+{
+   vector<double> entropyHold;
+   char selectedTask = ' '; //the current task we are selecting the entropy for
+   int extraFreq = 0; //the frequencies paired with the selectedTask
+   double currTerm = 0.00; //currentTerm = freq[selectedTask] * log2(freq[selectedTask])
+   double newTerm = 0.00; //newTerm = (freq[selectedTask] + extraFreq) * log2(freq[selectedTask] + extraFreq)
+   int currFreq = 0; //the sum of all previous frequencies
+   double currEntropy = 0.00; //initally 0, converts to the most recently created entropy
+   double entropy = 0.00; //the entropy for the selectedTask and the extraFrequency
+   int nFreq = 0; //the sum of all previous frequencies + the frequency paired with the selectedTask
+   ostringstream oss; //stitch output correctly
+   unordered_map<char, int> freqArray; //the frequency Array
+   for(const auto &it : entropyVector) { freqArray.insert({it.first, 0}); }
+   for(int x = 0; x < entropyVector.size(); x++) //for each task
+   {
+      selectedTask = entropyVector[x].first; //initialize selectedTask
+      extraFreq = entropyVector[x].second; //initialize extraFrequency
+      //begin the algorithm
+      nFreq = currFreq + extraFreq;
+      if(nFreq == extraFreq) { entropy = 0; } //for the first loop, the entropy will always be 0
+      else
+      {
+         if( freqArray[selectedTask] == 0) { currTerm = 0; }
+         else { currTerm = freqArray[selectedTask] * log2(freqArray[selectedTask]); }
+         newTerm = (freqArray[selectedTask] + extraFreq) * log2(freqArray[selectedTask] + extraFreq);
+         entropy = log2(nFreq) - ((log2(currFreq) - currEntropy) * currFreq - currTerm + newTerm)/nFreq;
+      }
+      entropyHold.push_back(entropy);
+      currFreq += extraFreq;
+      freqArray[selectedTask] += extraFreq;
+      currEntropy = entropy;
+      oss << fixed << setprecision(2) << entropy << " ";
+   }
+   string answer = oss.str();
+   answer.pop_back();
+   return answer;
+}
+
+int main(int argc, char *argv[])
+{
+   int sockfd, newsockfd, portno, clilen;
+   struct sockaddr_in serv_addr, cli_addr;
+   // Check the commandline arguments
+   if (argc != 2) { cerr << "Port not provided" << endl; exit(0); }
+   // Create the socket
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   if (sockfd < 0) { cerr << "Error opening socket" << endl; exit(0); }
+   // Populate the sockaddr_in structure
+   bzero((char *)&serv_addr, sizeof(serv_addr));
+   portno = atoi(argv[1]);
+   serv_addr.sin_family = AF_INET;
+   serv_addr.sin_addr.s_addr = INADDR_ANY;
+   serv_addr.sin_port = htons(portno);
+   // Bind the socket with the sockaddr_in structure
+   if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) { cerr << "Error binding" << endl; exit(0); }
+   // Set the max number of concurrent connections
+   listen(sockfd, 5);
+   clilen = sizeof(cli_addr);
+   signal(SIGCHLD, fireman); //call fireman to kill zombie processes
+   while(true) //for however many threads we will be recieiving
+   {
+      // Accept a new connection
+      newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, (socklen_t *)&clilen);
+      if(fork() == 0) //fork for every new thread
+      {
+         if (newsockfd < 0) { cerr << "Error accepting new connections" << endl; exit(0); }
+         int msgSize = 0;
+         //read message from client
+         if (read(newsockfd, &msgSize, sizeof(int)) < 0) { cerr << "Error reading size from socket" << endl; exit(0); }
+         char *tempBuffer = new char[msgSize + 1];
+         bzero(tempBuffer, msgSize + 1);
+         if (read(newsockfd, tempBuffer, msgSize + 1) < 0) { cerr << "Error reading string from socket" << endl; exit(0); }
+         string buffer = tempBuffer;
+         delete[] tempBuffer;
+         //preparing output to send to client
+         stringstream b(buffer);
+         vector<pair<char, int>> entropyVector;
+         char x; int y;
+         while(b >> x >> y) { entropyVector.push_back(make_pair(x, y)); }
+         string newBuffer = calculateEntropy(entropyVector);
+         msgSize = newBuffer.size();
+         //write message to client
+         if (write(newsockfd, &msgSize, sizeof(int)) < 0) { cerr << "Error writing size to socket" << endl; exit(0); }
+         if (write(newsockfd, newBuffer.c_str(), msgSize) < 0) { cerr << "Error writing string to socket" << endl; exit(0); }
+      }
+   }
+   close(newsockfd);
+   close(sockfd);
+   return 0;
+}
